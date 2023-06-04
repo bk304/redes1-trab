9a36c3537a794a914c8e95454c08790bd853be02
XNEP1 <xnep1@hotmail.com>
2023-06-04 20:56:12 -0300

    restore backup

--- Server.c
+++ Server.c
@@ -10,11 +10,21 @@
 #include "ConexaoRawSocket.h"
 #include "message.h"
 
+// enum estados {
+//     RECEBENDO_BACKUP,
+//     ENVIADO_BACKUP,
+//     ESPERANDO_COMANDO
+// }
+
 int main(void) {
     int socket = ConexaoRawSocket("lo");
     void *packet_buffer = malloc(PACKET_SIZE_BYTES * sizeof(unsigned char));
     t_message *message = init_message(packet_buffer);
     int read_status = 0;
+
+    // FILE *curr_file;
+
+    // int estado = ESPERANDO_COMANDO;
 
     printf("Lendo socket...\n");
     for (;;) {
