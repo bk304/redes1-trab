428311a2b6110b026b0129ab90e5bc6895eb3e8f
XNEP1 <xnep1@hotmail.com>
2023-06-07 14:51:48 -0300

    wip

--- Client.c
+++ Client.c
@@ -42,103 +42,129 @@ unsigned long hash_function(char *str) {
     return hash_value;
 }
 
-int identifica_comando(char *arg, int qnt) {
-    switch (hash_function(arg)) {
+int identifica_comando(char *argv[], int argc) {
+
+    int comando = -1;
+
+    switch (hash_function(argv[1])) {
         case BACKUP:
-            if (qnt == 2)
-                return C_BACKUP_1FILE;
-            else if (qnt > 2)
-                return C_BACKUP_GROUP;
+            if (argc == 3)
+                comando = C_BACKUP_1FILE;
+            else if (argc > 3)
+                comando = C_BACKUP_GROUP;
             else
-                return 0;
+                comando = -1;
+            break;
 
         case REC:
-            if (qnt == 2)
-                return C_BACKUP_1FILE;
-            else if (qnt > 2)
-                return C_BACKUP_GROUP;
+            if (argc == 3)
+                comando = C_RECOVER_1FILE;
+            else if (argc > 3)
+                comando = C_RECOVER_GROUP;
             else
-                return 0;
+                comando = -1;
+            break;
 
         case CDSV:
-            return CDSV;
+            comando = CDSV;
+            break;
 
         case VERIFICAR:
-            return VERIFICAR;
+            comando = VERIFICAR;
+            break;
 
         case CD:
-            return CD;
+            comando = CD;
+            break;
 
         default:
-            return 0;
+            comando = -1;
+            break;
     }
 
-    return 0;
+    return comando;
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 1) {
+    if (argc <= 1) {
         printf("Modo de Uso:\n\t$ client [backup|rec|cdsv|verificar|cd] [arquivos]\n");
         exit(0);
     }
 
-    int socket = ConexaoRawSocket(NETINTERFACE);
+    //int socket = ConexaoRawSocket(NETINTERFACE);
     void *packet_buffer = malloc(PACKET_SIZE_BYTES * sizeof(unsigned char));
     t_message *message = init_message(packet_buffer);
     int bytes_written;
 
     int estado = PROMPT_DE_COMANDO;
-    int comando = 0;
+    int comando = identifica_comando(argv, argc);
 
-    for (;;) {
-        // identifica o comando
-        switch (hash_function(argv[1])) {
-            case BACKUP:
-                if (argc == 2)
-                    comando = C_BACKUP_1FILE;
-                else if (argc > 2)
-                    comando = C_BACKUP_GROUP;
-                else
-                    comando = 0;
+    for(;;){
+        switch(estado){
+            //
+            case PROMPT_DE_COMANDO:
 
-            case REC:
-                if (argc == 2)
-                    comando = C_BACKUP_1FILE;
-                else if (argc > 2)
-                    comando = C_BACKUP_GROUP;
-                else
-                    comando = 0;
+                switch(comando){
+                    case C_BACKUP_1FILE:
+                        break;
 
-            case CDSV:
-                comando = CDSV;
+                    case C_BACKUP_GROUP:
+                        break;
 
-            case VERIFICAR:
-                comando = VERIFICAR;
+                    case C_RECOVER_1FILE:
+                        break;
 
-            case CD:
-                comando = CD;
+                    case C_RECOVER_GROUP:
+                        break;
 
-            default:
-                comando = 0;
-
-                if (!(comando = identifica_comando(argv[1], argc))) {
-                    exit(0);
-                    // continue;
+                    case 
                 }
 
-                printf("%d\n", comando);
+                break;
+
+            
+            //
+            case ENVIANDO_BACKUP_FILE:
+                break;
+
+            
+            //
+            case ENVIANDO_BACKUP_GROUP:
+                break;
+
+
+            //
+            case RECEBENDO_REC_FILE:
+                break;
+
+
+            //
+            case RECEBENDO_REC_GROUP:
+                break;
+
+            
+            //
+            case ESPERANDO_MD5:
                 break;
         }
-        exit(0);
-
-        // bytes_written = send_message(socket, message, 0, C_UNUSED_1, (void *)str, strlen(str));
-
-        if (bytes_written == -1) {
-            printf("ERRO NO WRITE\n    %s\n", strerror(errno));
-            exit(-1);
-        } else {
-            printf("%d bytes escritos.\n", bytes_written);
-        }
-
-        return 0;
     }
+
+    if (comando == -1) {
+        printf("ruim\n");
+        exit(-1);
+    }
+
+    printf("%d\n", comando);
+    exit(0);
+
+    // bytes_written = send_message(socket, message, 0, C_UNUSED_1, (void *)str, strlen(str));
+
+    if (bytes_written == -1) {
+        printf("ERRO NO WRITE\n    %s\n", strerror(errno));
+        exit(-1);
+    } else {
+        printf("%d bytes escritos.\n", bytes_written);
+    }
+
+    return 0;
+}
